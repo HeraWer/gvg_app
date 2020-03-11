83b81fd17b3f7743218717d5ddd8b4f70592e092
adriangc24 <adriangcamacho24@gmail.com>
2020-03-11 18:16:49 +0100

    ppppp

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,26 +1,11 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var email,password,
 var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
+
+    
 
     // deviceready Event Handler
     //
@@ -48,6 +33,28 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  $("#btnLogin").click(function(){
+    email = $("#email").val();
+    password = $("#password").val();
+    comsole.log(email, pass);
+
+    logIn();
+  });
+
+  function logIn (username,password){
+      console.log("Log In");
+    const request = require("request-promise"),
+    //RUTA = "https://app-intercruises.herokuapp.com/login";
+    RUTA = "http://localhost:3000/login"
+request({
+    uri: RUTA,
+    json: true, // Para que lo decodifique automáticamente 
+}).then(
+    username,
+    password
+);
   }
 
 app.initialize();
