1d7880cdffc125ccce7bf63941cde0a8c0269306
adriangc24 <adriangcamacho24@gmail.com>
2020-03-18 15:47:13 +0100

    doing slide menu

--- www/js/index.js
+++ www/js/index.js
@@ -1,100 +1,103 @@
 
-        $(document).ready(function(){
-          $('.errorLogin').hide();
-          $('#btnLogin').click(function(){
-            logIn();
-          });
-        });
+$(document).ready(function(){
+  $('.errorLogin').hide();
+  $('#btnLogin').click(function(){
+    logIn();
+});
+});
 
 
-        var app = {
-            // Application Constructor
-            initialize: function() {
-                document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-            },
+var app = {
+                // Application Constructor
+                initialize: function() {
+                    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+                },
 
-            // deviceready Event Handler
-            //
-            // Bind any cordova events here. Common events are:
-            // 'pause', 'resume', etc.
-            onDeviceReady: function() {
-                this.receivedEvent('deviceready');
-            },
+                // deviceready Event Handler
+                //
+                // Bind any cordova events here. Common events are:
+                // 'pause', 'resume', etc.
+                onDeviceReady: function() {
+                    this.receivedEvent('deviceready');
+                },
 
-            // Update DOM on a Received Event
-            receivedEvent: function(id) {
-                var parentElement = document.getElementById(id);
-                var listeningElement = parentElement.querySelector('.listening');
-                var receivedElement = parentElement.querySelector('.received');
+                // Update DOM on a Received Event
+                receivedEvent: function(id) {
+                    var parentElement = document.getElementById(id);
+                    var listeningElement = parentElement.querySelector('.listening');
+                    var receivedElement = parentElement.querySelector('.received');
 
-                listeningElement.setAttribute('style', 'display:none;');
-                receivedElement.setAttribute('style', 'display:block;');
+                    listeningElement.setAttribute('style', 'display:none;');
+                    receivedElement.setAttribute('style', 'display:block;');
 
-                console.log('Received Event: ' + id);
-            }
-
-        }
-        /*
-        function onSignIn(googleUser) {
-            var profile = googleUser.getBasicProfile();
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-          }
-        */
-
-            
-
-          function logIn (){
-            console.log("entra function login jquery");
-            if($("#inputPass").val()==""&&$("#inputEmail").val()==""){
-                console.log("Email and password empty");
-                $('.errorLogin').hide();
-                $('#errorEmailPass').show();
-            }
-            /*else if(!$("#inputEmail").val().includes('@')&&!$("#inputEmail").val().includes('.')){
-                console.log("Email invalido");
-                $('.errorLogin').hide();
-                $('#errorEmail').show();
-
-            }*/
-            else if($("#inputPass").val()==""){
-                console.log("Password invalido");
-                $('.errorLogin').hide();
-                $('#errorPass').show();
+                    console.log('Received Event: ' + id);
+                }
 
             }
-            else {
+            /*
+            function onSignIn(googleUser) {
+                var profile = googleUser.getBasicProfile();
+                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                console.log('Name: ' + profile.getName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+              }
+              */
+
+
+
+              function logIn (){
+                console.log("entra function login jquery");
+                if($("#inputPass").val()==""&&$("#inputEmail").val()==""){
+                    console.log("Email and password empty");
+                    $('.errorLogin').hide();
+                    $('#errorEmailPass').show();
+                }
+                /*else if(!$("#inputEmail").val().includes('@')&&!$("#inputEmail").val().includes('.')){
+                    console.log("Email invalido");
+                    $('.errorLogin').hide();
+                    $('#errorEmail').show();
+
+                }*/
+                else if($("#inputEmail").val()==""){
+                    console.log("Email invalido");
+                    $('.errorLogin').hide();
+                    $('#errorEmail').show();
+
+                }
+                else if($("#inputPass").val()==""){
+                    console.log("Password invalido");
+                    $('.errorLogin').hide();
+                    $('#errorPass').show();
+
+                }
+                else {
                     $('.errorLogin').hide();
                     email = $("#inputEmail").val();
                     password = $("#inputPass").val();
 
                     var userName = '{"username":"'+email+'", "password":"'+password+'"}';
                     var parseUserName = JSON.parse(userName); 
-                    //RUTA = "https://app-intercruises.herokuapp.com/login";
-            
-                    $.ajax({
-                type: "POST",
-                //url: 'https://app-intercruises.herokuapp.com/login',
-                url: 'http://localhost:3000/login',
-                data: parseUserName,
-                crossDomain: true,
-                dataType: 'json',
-                //data: '{"username":"'+email+'", "password:":"'+password+'"}',
-                succes: function() {
-                    console.log("SUCCESS !");
-                } 
-            })
-            .fail(function () {
-                M.toast({
-                    html: 'No se pudo establecer conexión con el servidor'
+                        //RUTA = "https://app-intercruises.herokuapp.com/login";
+
+                        $.ajax({
+                            type: 'POST',
+                            url: 'https://app-intercruises.herokuapp.com/login',
+                    //url: 'http://localhost:3000/login',
+                    data: parseUserName,
+                    crossDomain: true,
+                    dataType: 'json',
+                    //data: '{"username":"'+email+'", "password:":"'+password+'"}',
+                    succes: function() {
+                        console.log("SUCCESS !");
+                    } 
                 })
-            })
-            }
+                        .fail(function () {
+                            M.toast({
+                                html: 'No se pudo establecer conexión con el servidor'
+                            })
+                        })
+                    }
+                }
 
-         
-
-            }
-
-        app.initialize();
+                app.initialize();
--- www/js/main_menu.js
+++ www/js/main_menu.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
+
+/*$(document).ready(function(){
+  $('.menuButton').click(function(){
+    $(this).css('background-color','yellow');
+});
+});*/
